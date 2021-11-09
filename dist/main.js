ac5ede600573f7e4ee1408ffa6486c0b1f712102
blessedjasonmwanza <mwanzabj@gmail.com>
2021-11-09 16:40:21 +0200

    Add src files structure

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+document.querySelector('body').innerHTML = 'welcome';
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHFEIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbGVhZGVyYm9hcmQvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiZG9jdW1lbnQucXVlcnlTZWxlY3RvcignYm9keScpLmlubmVySFRNTCA9ICd3ZWxjb21lJzsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
