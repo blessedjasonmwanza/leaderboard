f5b32b1d242f738008ba866bdd38a4a113777f46
blessedjasonmwanza <mwanzabj@gmail.com>
2021-11-10 00:42:45 +0200

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ const leaderboard = new Scores();
 leaderboard.displayScores();
 // refresh btn
 document.querySelector('.refresh').addEventListener('click', () => {
-    leaderboard.displayScores();
+  leaderboard.displayScores();
 });
 // form submission
 const form = document.querySelector('.add-score');
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const name = form.elements.name.value;
-    const score = form.elements.score.value;
-    leaderboard.addParticipant(name, score).then((res) => {
-        if(!res.error){
-            form.reset();
-        }
-    });
+  e.preventDefault();
+  const name = form.elements.name.value;
+  const score = form.elements.score.value;
+  leaderboard.addParticipant(name, score).then((res) => {
+    if (!res.error) {
+      form.reset();
+    }
+  });
 });
--- src/leaderboard.js
+++ src/leaderboard.js
@@ -1,40 +1,42 @@
 export default class Scores {
   constructor() {
     this.endpoint = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
-    this.id = "Bf6jsb0iOiep8n3zEeum";
+    this.id = 'Bf6jsb0iOiep8n3zEeum';
     this.participants = [];
   }
-  async getScores () {
-    return await fetch(this.endpoint+this.id+'/scores/')
-    .then((res) => res.json())
-    .then((data) => ({error: false, participants: data.result}))
-    .catch((error) => ({error: true, participants: this.participants}));
+
+  async getScores() {
+    const response = await fetch(`${this.endpoint + this.id}/scores/`)
+      .then((res) => res.json())
+      .then((data) => ({ error: false, participants: data.result }))
+      .catch(() => ({ error: true, participants: this.participants }));
+    return response;
   }
 
   async addParticipant(name, score) {
-    if(name && score && !isNaN(score) && typeof name === 'string'){
-      return await fetch(this.endpoint+this.id+'/scores/', {
+    if (name && score && !Number.isNaN && typeof name === 'string') {
+      const response = await fetch(`${this.endpoint + this.id}/scores/`, {
         method: 'POST',
         body: JSON.stringify({
           user: name,
-          score: score
+          score,
         }),
         headers: {
-          'Content-Type':'application/json'
-        }
+          'Content-Type': 'application/json',
+        },
       })
-      .then((res) => res.json())
-      .then((msg) => ({error: false, msg: msg}))
-      .catch((error) => ({error: true, msg: "Score could not be added"}));
-    }else{
-      return ({error: true, msg: 'Invalid name or score value'});
+        .then((res) => res.json())
+        .then((msg) => ({ error: false, msg }))
+        .catch(() => ({ error: true, msg: 'Score could not be added' }));
+      return response;
     }
+    return ({ error: true, msg: 'Invalid name or score value' });
   }
 
   displayScores() {
     this.getScores()
-    .then((res) => {
-        if(!res.error){
+      .then((res) => {
+        if (!res.error) {
           this.participants = res.participants;
           const scoresDomList = document.querySelector('.scores-list');
           scoresDomList.innerHTML = '';
@@ -42,6 +44,6 @@ export default class Scores {
             scoresDomList.innerHTML += `<li>${participant.user}: ${participant.score}</li>`;
           });
         }
-    });
+      });
   }
 }
